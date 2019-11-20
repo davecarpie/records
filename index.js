23fd9eda495464bf35172fc5b82bbe86e44f66bc
Dave Carpenter <me@davecarpie.com>
2019-11-19 17:39:29 -0800

    Primitive filtering
    Add super primitive string filtering using includes.
    
    Searching for "lor" shows "Color and the Shape" above an album by "Lorde"

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ Vue.component('record', {
       <h1>{{ record.title}}</h1>
       <h2>{{ record.artist }}</h2>
       <img v-bind:src="record.artworkUrl" />
+    </div>
     `
 })
 
 var app = new Vue({
   el: '#app',
   data: {
+    filterText: "",
     records: [
       { title: "The Color and the Shape", artist: "Foo Fighters", artworkUrl: "https://upload.wikimedia.org/wikipedia/en/0/0d/FooFighters-TheColourAndTheShape.jpg"},
       { title: "Melodrama", artist: "Lorde", artworkUrl: "https://upload.wikimedia.org/wikipedia/en/b/b2/Lorde_-_Melodrama.png" }
     ]
+  },
+  computed: {
+    filteredRecords: function() {
+      return this.records.filter(r => r.title.toLowerCase().includes(this.filterText.toLowerCase()) || r.artist.toLowerCase().includes(this.filterText.toLowerCase()))
+    }
   }
 })
